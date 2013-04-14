f15ecdcd5e8cdc480ee31be706c4a01060f497f7
Star Brilliant <m13253@hotmail.com>
2013-04-14 13:53:34 +0800

    sys.exit() to return

--- lib/yats/main.py
+++ lib/yats/main.py
@@ -40,21 +40,21 @@ def run():
     (options, args)=optparser.parse_args()
     if options.port==None:
         logging.error('Port number must be specified.')
-        sys.exit(1)
+        return 1
     if options.key==None:
         logging.error('Password must be specified.')
-        sys.exit(1)
+        return 1
     elif not 0<options.port<65535:
         logging.error('Port number must be between 0 and 65535.')
-        sys.exit(1)
+        return 1
     elif options.daemon:
-        sys.exit(ServerPeer([(options.bind, options.port)], options.key).loop())
+        return ServerPeer([(options.bind, options.port)], options.key).loop()
     elif len(args)!=1:
         logging.error('You must specify a server to connect to.')
-        sys.exit(1)
+        return 1
     elif len(options.local)==0 and len(options.remote)==0 and len(options.dynamic)==0:
         logging.error('You must specify one of -L, -R or -D.')
-        sys.exit(1)
+        return 1
     else:
         client_peer=ClientPeer(options.bind, (args[0], options.port), options.key)
         for i in options.local:
@@ -63,5 +63,5 @@ def run():
             client_peer.parse_tunnel(1, i)
         for i in options.dynamic:
             client_peer.parse_tunnel(2, i)
-        sys.exit(client_peer.loop())
+        return client_peer.loop()
 
